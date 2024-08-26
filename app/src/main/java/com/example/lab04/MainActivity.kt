15550307801e77b7aaad2622b6d08b9bfa1c69d3
Time <andregonzalesticona@gmail.com>
2024-08-26 16:53:10 -0500

    add a new CustomButton

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +33,12 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    // Uso del componente CustomButton dentro del Scaffold
+                    CustomButton(
+                        onClick = { /* Acción del botón */ },
+                        text = "Presionar"
+                    )
+
                 }
             }
         }
@@ -33,15 +48,36 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "",
         modifier = modifier
     )
 }
 
+@Composable
+fun CustomButton(onClick: () -> Unit, text: String) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF6200EE)),
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier
+            .padding(100.dp)
+            .fillMaxSize()
+            .height(10.dp)
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+ //para poder tener una vista previa del codigo
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab04Theme {
         Greeting("Android")
+        // También puedes previsualizar el botón
+        CustomButton(onClick = {}, text = "Presionar")
     }
 }
